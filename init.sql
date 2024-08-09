aa195635bf32ad80101c110f2012b51c0b1b542a
nshelevoy <nshelevoy.pex@sanitas.es>
2024-08-09 20:08:19 +0200

    sql and yml update

--- init.sql
+++ init.sql
@@ -1,1 +1,9 @@
-CREATE SCHEMA cybindev_schema;
+-- Crear esquema cybinschema
+CREATE SCHEMA cybinschema;
+
+-- (Opcional) Crear una tabla en el esquema
+CREATE TABLE cybinschema.users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL
+);

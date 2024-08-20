d2ced658f2010ce916840a47d5df9605fc11e0ca
nshelevoy <nshelevoy.pex@sanitas.es>
2024-08-20 13:22:20 +0200

    tables creation modified

--- init.sql
+++ init.sql
@@ -1,9 +1,22 @@
--- Crear esquema cybinschema
+-- 1.Crear esquema cybinschema
 CREATE SCHEMA cybinschema;
 
--- (Opcional) Crear una tabla en el esquema
+-- 2.Crear la tabla de roles en el esquema
+CREATE TABLE cybinschema.roles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+-- 3.Crear la tabla de usuarios en el esquema
 CREATE TABLE cybinschema.users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- 4.Crear la tabla de relaciones muchos a muchos entre usuarios y roles
+CREATE TABLE cybinschema.user_roles (
+    user_id INT REFERENCES cybinschema.users(id) ON DELETE CASCADE,
+    roles_id INT REFERENCES cybinschema.roles(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, roles_id)
 );
